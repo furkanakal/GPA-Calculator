06b68127cc36ba1500ce5a9bb5f4672f52d9f4ec
Furkan <furkanakaldev@gmail.com>
2023-02-25 22:59:27 +0300

    add: struct Class3

--- CGPAView.swift
+++ CGPAView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct CGPAView: View {
-    var body: some View {
+	@State var classes: [Class3] = []
+	
+	
+	var body: some View {
+		NavigationView {
 			
-    }
+		}
+	}
+}
+
+func calculateCGPA(classes: [Class3]) -> Double {
+	var
+}
+
+struct Class3: Identifiable {
+	var id: UUID
+	var name: String
+	var gpa: Double
 }
 
 struct CGPAView_Previews: PreviewProvider {
-    static var previews: some View {
-        CGPAView()
-    }
+	static var previews: some View {
+		CGPAView()
+	}
 }
